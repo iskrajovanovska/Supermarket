345f1e2d03c19caa6fc3302c2c4c88164813c007
Iskra Jovanovska <iskra.matf@gmail.com>
2016-09-07 09:33:24 +0200

    pokusaj1

---
+++ Supermarket/app/src/main/java/com/example/cubesschool7/supermarket/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cubesschool7.supermarket.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //lalala
+}
