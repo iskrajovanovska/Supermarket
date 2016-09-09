3f845f00942a24094bbb740cfa21413e39239fb1
Iskra Jovanovska <iskra.matf@gmail.com>
2016-09-09 09:13:46 +0200

    kod od kuce

--- Supermarket/app/src/main/java/com/example/cubesschool7/supermarket/main/LoginActivity.java
+++ Supermarket/app/src/main/java/com/example/cubesschool7/supermarket/main/LoginActivity.java
@@ -1,27 +1,99 @@
 package com.example.cubesschool7.supermarket.main;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * Created by cubesschool7 on 9/7/16.
  */
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText editTextLozinka, editTextKorisnik;
+    private ImageView logo;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        editTextLozinka = (EditText) findViewById(R.id.editLozinka);
-        editTextKorisnik = (EditText) findViewById(R.id.editKorisnickoIme);
+        tabLayout = (TabLayout) findViewById(R.id.viewTabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viePager1);
+        logo = (ImageView) findViewById(R.id.logo);
 
-        editTextLozinka.getBackground().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
-        editTextKorisnik.getBackground().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation);
+
+
+        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+        });
+    }
+
+    private class CustomAdapter extends FragmentPagerAdapter {
+        private String [] fragments = {"PRIJAVA","REGISTRACIJA"};
+
+        public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new Fragment1();
+                case 1:
+                    return new Fragment2();
+                default:
+                    return null;
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragments[position];
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+       logo.startAnimation(animation);
     }
 }
--- Supermarket/app/src/main/java/com/example/cubesschool7/supermarket/main/StartActivity.java
+++ Supermarket/app/src/main/java/com/example/cubesschool7/supermarket/main/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.cubesschool7.supermarket.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,9 +10,33 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class StartActivity extends AppCompatActivity {
 
+    private Thread mSplashThread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        mSplashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        wait(2000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
+
+                finish();
+
+                Intent intent = new Intent();
+                intent.setClass(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        mSplashThread.start();
     }
+
 }
